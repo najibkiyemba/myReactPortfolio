4cc1c0f2887cbdfe8fc2aa5a76f854dc3e612c5a
Najib Kaur <najibkaur@Najibs-MacBook-Pro.local>
2023-01-07 14:22:47 -0800

    changes to resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Resume from '../../assets/najib@Resume.pdf';
+//import Resume from '../../assets/najib@Resume.pdf';
 //import UpdatedResume from '../updatedResume/UpdatedResume';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://najibkiyemba.github.io/updatedResume2023/" rel="link" className="btn">
+      <a target="_blank" rel="noreferrer" href="https://najibkiyemba.github.io/updatedResume2023/"  className="btn">
         View Resume
       </a>
       <a href="#contact" className="btn btn-primary">
